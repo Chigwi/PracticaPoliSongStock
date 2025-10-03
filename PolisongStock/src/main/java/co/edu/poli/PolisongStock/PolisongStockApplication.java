c2026ef5747807d042d0e9d2c333a408a2e05b6e
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-10-03 11:39:04 -0500

    creación del proyecto de polisong stock

---
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/PolisongStockApplication.java
@@ -0,0 +1,13 @@
+package co.edu.poli.PolisongStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisongStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolisongStockApplication.class, args);
+	}
+
+}
---
+++ PolisongStock/src/test/java/co/edu/poli/PolisongStock/PolisongStockApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.poli.PolisongStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolisongStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
