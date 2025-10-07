5f82a58aa7e6daabe7ff38e6a0840bdb95245c15
salitooooo <salodorado2004@gmail.com>
2025-10-07 16:18:52 -0500

    cancion

--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/modelo/Cancion.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/modelo/Cancion.java
@@ -1,6 +1,14 @@
 package co.edu.poli.PolisongStock.modelo;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,11 +23,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Cancion {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCancion;
+	
+	@Column(name="nombre")
 	private String nombre;
+	
+	@Column(name="artista")
 	private String artista;
+	
+	@Column(name="annoPublicacion")
 	private String annoPublicaion;
+	
+	@Column(name="precio")
 	private Double precio;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "formato")
+	@Column(name="formato")
 	private Formato formato;
 	
 }
