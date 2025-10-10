37123a51589f8b00566a710e60a6c5e2f1f2e0ba
salitooooo <salodorado2004@gmail.com>
2025-10-10 12:50:08 -0500

    cancion config #2

--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/Cancion/repository/CancionRepository.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/Cancion/repository/CancionRepository.java
@@ -1,4 +1,4 @@
-package co.edu.poli.PolisongStock.Cancion.Repository;
+package co.edu.poli.PolisongStock.Cancion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
