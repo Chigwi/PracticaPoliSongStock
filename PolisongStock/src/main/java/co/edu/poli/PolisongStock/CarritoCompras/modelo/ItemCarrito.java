77224df8393688204eb373d5c11ff0c09e0357af
salitooooo <salodorado2004@gmail.com>
2025-10-14 14:53:19 -0500

    repository

--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/modelo/ItemCarrito.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/modelo/ItemCarrito.java
@@ -1,12 +1,18 @@
 package co.edu.poli.PolisongStock.CarritoCompras.modelo;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "Itemcarrito")
+@Data
 public class ItemCarrito {
 	
     @Id
---
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/repository/ItemCarritoRepository.java
@@ -0,0 +1,11 @@
+package co.edu.poli.PolisongStock.CarritoCompras.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.poli.PolisongStock.CarritoCompras.modelo.ItemCarrito;
+
+
+
+public interface ItemCarritoRepository extends JpaRepository<ItemCarrito, Long>{
+
+}
