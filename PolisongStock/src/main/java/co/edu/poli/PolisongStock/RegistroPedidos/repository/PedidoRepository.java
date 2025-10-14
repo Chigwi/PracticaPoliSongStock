b5f692c3401ba0e03eeb8163fd3baccfdd019b4e
Jessicxx <jessicalombo4@gmail.com>
2025-10-14 15:38:31 -0500

    UwU más errores en la query uwu

--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/RegistroPedidos/repository/PedidoRepository.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/RegistroPedidos/repository/PedidoRepository.java
@@ -1,5 +1,8 @@
 package co.edu.poli.PolisongStock.RegistroPedidos.repository;
 
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import co.edu.poli.PolisongStock.RegistroPedidos.modelo.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	@Query("SELECT p.idPedido, p.ExperienciaId.calificacion, p.Experiencia.descripcion FROM Pedido p WHERE p.idpedido = :id")
+	public List<Pedido> findByObtenerPedidosConExperiencia(Long id);
 
 }
