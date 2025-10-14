0252a21a301b8841e4d026d1ef186f7352e76e3c
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-10-14 13:38:29 -0500

    esqueleto carrito compras

--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/controller/CarritoComprasController.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/controller/CarritoComprasController.java
@@ -1,8 +1,18 @@
 package co.edu.poli.PolisongStock.CarritoCompras.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.poli.PolisongStock.CarritoCompras.modelo.CarritoCompras;
 
 @RestController
 public class CarritoComprasController {
+	
+	@PostMapping
+	public ResponseEntity<CarritoCompras> createPedido(){
+		CarritoCompras r = new CarritoCompras();
+		return ResponseEntity.ok(r);
+	}
 
 }
--- PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/modelo/CarritoCompras.java
+++ PolisongStock/src/main/java/co/edu/poli/PolisongStock/CarritoCompras/modelo/CarritoCompras.java
@@ -1,8 +1,16 @@
 package co.edu.poli.PolisongStock.CarritoCompras.modelo;
 
+import co.edu.poli.PolisongStock.RegistroPedidos.modelo.Pedido;
+import co.edu.poli.PolisongStock.RegistroUsuario.modelo.Persona;
 import lombok.Data;
 
 @Data
 public class CarritoCompras {
-
+	
+	private Persona usuario;
+	
+	private Pedido pedido;
+	
+	private Integer precioTotal;
+	
 }
